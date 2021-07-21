8685a5694ca5c8c80be6bf0e14299c53ef348e90
ubuntu <deinfochips@gmail.com>
2021-07-21 10:25:42 -0700

    Jenkinsfile

---
+++ vars/CI.groovy
@@ -0,0 +1,51 @@
+def call() {
+pipeline {
+    agent any
+    // options {
+    //     timeout(time: 1, unit: 'MINUTES') 
+    //     }
+    parameters {
+            choice(name: 'Colour', choices: ['blue', 'green','black','white'], description: 'select colour')
+            booleanParam(name: 'colour-black', defaultValue: true, description: 'check if colour is not black')
+            string(name: 'hello', defaultValue: 'divya', description: 'name is divya')
+        }
+    stages {
+		
+        stage('Build') {
+            steps {
+			   
+               sh "cd sfg-pet-clinic && mvn clean install"
+            }
+        }
+	    stage('Build docker image') {
+			steps{
+				sh '''docker build --tag jira:diya1 .
+					'''
+				}
+			}
+        stage('master-branch-stuff') {
+                when {
+                    branch 'newbranch*'
+                }
+                steps {
+                    echo 'run this stage - ony if the branch = master branch'
+                }
+        }
+		stage('Clone additional repository') {
+                steps {
+                    dir('newdir') {
+                        git branch: 'main', credentialsId: '499d84ac-7790-4571-998a-2f3eed2b292e', url: 'https://github.com/divz141/hello-world.git'
+                    }
+                }
+		}
+        stage('Build docker image second repo') {
+			steps {
+				sh '''cd newdir
+					  docker build --tag jira:diya2 .
+				    '''
+			}
+		}
+		
+    }
+}
+}
